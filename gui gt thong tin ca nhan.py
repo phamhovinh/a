596d8c53108b8a703dc1d6b5a02da2c9d1c76a79
phamhovinh <78393538+phamhovinh@users.noreply.github.com>
2021-05-29 08:39:10 +0700

    Add files via upload

---
+++ gui gt thong tin ca nhan.py
@@ -0,0 +1,8 @@
+from tkinter import *
+window=Tk()
+window.title("gthieu thong tin ca nhan")
+window.geometry('350x200')
+Label(window,text="phamhovinh").pack()
+Label(window,text="19575202160015").pack()
+Label(window,text="Tu dong hoa").pack()
+window.mainloop()
